9d114d4a7c20bab83a9fd09b709735408abd7d75
Leighton Meredith <luluket2000@gmail.com>
2025-03-17 13:22:49 -0400

    add new coach profile pic

--- src/app/coaches/coaches.component.ts
+++ src/app/coaches/coaches.component.ts
@@ -15,6 +15,7 @@ export class CoachesComponent {
     {src: '../assets/img/coaches/leighton.jpg', name: `Leighton 'M' Meredith`, title: 'Coach', alt: 'm-profile'},
     {src: '../assets/img/coaches/dwayne.jpg', name: `Dwayne 'Zaro' Thomas`, title: 'Coach', alt: 'zaro-profile'},
     {src: '../assets/img/coaches/malcolm.jpg', name: `Shomari Malcolm`, title: 'Coach', alt: 'malcolm-profile'},
+    {src: '../assets/img/coaches/fabian.jpg', name: `Fabian Lewis`, title: 'Coach', alt: 'fabian-profile'},
     {src: '../assets/img/coaches/shana.jpg', name: `Shana Langley`, title: 'Academy Coordinator', alt: 'shana-profile'}
   ]
 }
