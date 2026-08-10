06ab801d922985bb4072a927036a33311595c31d
Leighton Meredith <luluket2000@gmail.com>
2026-08-10 16:26:26 -0400

    remove unused import

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-about',
-    imports: [],
-    templateUrl: './about.component.html',
-    styleUrl: './about.component.scss'
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.scss',
 })
-export class AboutComponent {
-
-}
+export class AboutComponent {}
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -4,28 +4,27 @@ import { RoutesConstants } from '../shared/utils/constants';
 import packageJson from '../../../package.json';
 
 @Component({
-    selector: 'app-footer',
-    imports: [],
-    templateUrl: './footer.component.html',
-    styleUrl: './footer.component.scss'
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
   private router = inject(Router);
   version = packageJson.version;
 
   goToHome() {
-    this.router.navigate([RoutesConstants.HOME])
+    this.router.navigate([RoutesConstants.HOME]);
   }
 
   goToAbout() {
-    this.router.navigate([RoutesConstants.ABOUT])
+    this.router.navigate([RoutesConstants.ABOUT]);
   }
 
   goToCoaches() {
-    this.router.navigate([RoutesConstants.COACHES])
+    this.router.navigate([RoutesConstants.COACHES]);
   }
 
   goToPrograms() {
-    this.router.navigate([RoutesConstants.PROGRAMS])
+    this.router.navigate([RoutesConstants.PROGRAMS]);
   }
 }
